72747a434d2b92aeb49919c075c448b3c3dbd3b5
Shivanshu981 <shivanshu981@gmail.com>
2018-06-20 10:21:49 -0700

    Futher changes are made and bugs are solved and you can make OOD File. Still in Fixing......

--- leap.py
+++ leap.py
@@ -12,6 +12,14 @@ def file_input(x,y):
     with open("file_001_#name.txt","a")as f1:
         f1.write(x+" created on "+y+"\n")
         f1.close
+
+def file_open(filename,date_time,Mode):
+    with open("file_001_#name.txt","a")as f1:
+        f1.write(filename+"open on"+date_time+" as "+Mode)
+
+def file_open2(filename,date_time,Mode):
+    with open("file_001_#name.txt","a")as f1:
+        f1.write(filename+"open on"+date_time+" as "+Mode)
 # ---------------------------------------------------        
 
 print("Welcome\nLeap 1.0.0v\nAn approch to create a Object Oriented Data by using python")
@@ -25,6 +33,7 @@ while True:
     print("Press 2 For OOD in JSON File")
     user_input = str(input("-->>> "))
     # Choose your file format
+    # In JSON Format
     if user_input=="1":
         while True:
             print("Press 1 For New File\n")
@@ -36,7 +45,7 @@ while True:
             if user_input2=="1":
                 user_input_filename = str(input("New File Name: "))
                 #Function call for take records of file
-                name_001 = file_input(user_input_filename,date_now)
+                file_input(user_input_filename,date_now)
                 
                 with open(user_input_filename+".json","w")as f:
                     dist = str(input("Collection/Table Name: "))
@@ -91,6 +100,7 @@ while True:
 
                                 elif user_input_option3=="n":
                                     f.write("db."+d+".insert("+str(dist)+")\n")
+                                    file_open(user_input_filename2,date_now,"Write Mode")
                                     f.close()
                                     break
                             
@@ -102,11 +112,13 @@ while True:
                 elif user_input_option2=="r":
                     with open(user_input_filename2+".json","r")as f:
                         print(f.read())
+                        file_open2(user_input_filename2,date_now,"Read Mode")
                         f.close()
                  
 
     elif user_input=="quit()":
         break
+
     # Object Oriented Database by JSON File
     elif user_input=="2":
         while True:
@@ -124,7 +136,16 @@ while True:
                     d=dist
                     key = str(input("Topic Name: "))
                     value = str(input("Value: "))
-                    dist={key:value}
+                    # Conversion in Object Data format
+                    key_formated1 = "Content:[{"+key+":{ "
+                    value_formated1 = value+"}"
+                    # Formated Values
+                    dist = {key_formated1:value_formated1}
+
+                    dist_str = str(dist)
+                    dist_str = dist_str.replace("'","")
+                    
+                        
                     
                     # Code for OOD is here
                     while True:
@@ -133,12 +154,17 @@ while True:
                         if user_input_option=="Y":
                             key = str(input("Topic Name: "))
                             value = str(input("Value: "))
-                            dist[key] = value
-                            print(str(dist))
+                            key_formated2 = key+':{ '
+                            value_formated2 = value+'}'
+                            # Formated Values
+                            dist[key_formated2] = value_formated2
+                            dist_str2 = str(dist)
+                            dist_str2 = dist_str2.replace("'","")
+                            print(str(dist_str2))
                             
                         
                         elif user_input_option=="n":
-                            f.write("db."+d+".insert("+str(dist)+")")
+                            f.write("db."+d+".insert("+str(dist)+")") # The change for Object Oriented Data is made here
                             break
                     
                 
